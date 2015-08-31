d54f5d36574cb6ca1b31ff577083fabca7642574
593615100 <593615100@qq.com>
2015-08-31 14:12:39 +0800

    注释掉测试代码

--- ljt_algorithm/test.cpp
+++ ljt_algorithm/test.cpp
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main()
-{
-	printf("------------------------------>");
+//int main()
+//{
+//	printf("------------------------------>");
+//
+//	return 0;
+//}
 
-	return 0;
-}
