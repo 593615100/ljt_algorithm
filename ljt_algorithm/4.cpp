fdb1d67ef8ada1a132ed5c775c51998cc008ff6c
593615100 <593615100@qq.com>
2015-08-31 15:59:05 +0800

    命令行测试

---
+++ ljt_algorithm/4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int quik_main()
+{
+	std::cout<<"ÅÅÐòÇ°"<<endl;
+
+	std::cout<<endl;
+
+
+	std::cout<<"ÅÅÐòºó"<<endl;
+
+	std::cout<<endl;
+	system("pause");
+	return 0;
+}
