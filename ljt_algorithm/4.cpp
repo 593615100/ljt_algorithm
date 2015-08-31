d4eb3ef14e645034083455dc0f7ef5ad9f7d3ed0
593615100 <593615100@qq.com>
2015-08-31 16:19:51 +0800

    汉字乱码

--- ljt_algorithm/4.cpp
+++ ljt_algorithm/4.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int quik_main()
 {
-	std::cout<<"排序之前"<<endl;
+	std::cout<<"排序前"<<endl;
 
 	std::cout<<endl;
 
 
-	std::cout<<"排序之后"<<endl;
+	std::cout<<"排序后"<<endl;
 
 	std::cout<<endl;
 	system("pause");
